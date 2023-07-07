fa1ad4462fcd1dbba2d1dfa1a35ca48a1cd6d7c8
Andry <andrijfodor@gmail.com>
2023-07-07 11:53:55 +0300

    Create first view of page

---
+++ src/Componens/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return(
+        <header className='header'>
+        <img src='https://e7.pngegg.com/pngimages/949/715/png-clipart-bmp-file-format-file-formats-bitmap-studio-one-icon-image-file-formats-trademark.png' alt='logo'></img>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/Componens/Profile.jsx
@@ -0,0 +1,35 @@
+
+const Profile = () =>{
+    return (
+        <div className='content'>
+        
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRisv-yQgXGrto6OxQxX62JyvyQGvRsQQ760g&usqp=CAU' alt='alt'></img>
+        
+        <div>
+          ava + text
+        </div>
+
+        <div>
+          My posts
+          <div>
+            New posts
+          </div>
+          <div>
+            <div>
+              Post1
+            </div>
+            <div>
+              Post2
+            </div>
+          </div>
+        </div>
+
+        <div>
+          coment
+        </div>
+
+      </div>
+    );
+}
+
+export default Profile;
