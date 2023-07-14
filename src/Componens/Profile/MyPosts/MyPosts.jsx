cdd9fb2ae1c809d8deffa3baf6fe8f094f039537
Andry <andrijfodor@gmail.com>
2023-07-14 21:33:11 +0300

    Created props in Post.jsx component

--- src/Componens/Profile/MyPosts/MyPosts.jsx
+++ src/Componens/Profile/MyPosts/MyPosts.jsx
@@ -10,10 +10,8 @@ const MyPosts = () =>{
           <div>My posts</div>
           <div>
             New posts
-            <Post />
-            <Post />
-            <Post />
-            <Post />
+            <Post message = 'Hi, how are you?' img = 'https://w7.pngwing.com/pngs/862/646/png-transparent-beard-hipster-male-man-avatars-xmas-giveaway-icon-thumbnail.png' alt = 'User1'/>
+            <Post message = "It`s my first post" img = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkfaknv6JbkcRppV4gFFlgeGFG3BX77i7uQH_RbGS1qghS__bN9CkixepsC9a69zCmyBI&usqp=CAU' alt = 'User2'/>
           </div>
         </div>
     );
--- src/Componens/Profile/MyPosts/Post/Post.jsx
+++ src/Componens/Profile/MyPosts/Post/Post.jsx
@@ -1,10 +1,10 @@
 import styles from "./Post.module.css"
 
-const Post = () =>{
+const Post = (props) =>{
     return (
         <div className={styles.item}>   
-            <img src="https://w7.pngwing.com/pngs/862/646/png-transparent-beard-hipster-male-man-avatars-xmas-giveaway-icon-thumbnail.png" alt="someUser" />
-            <span>Text of this post</span>
+            <img src={props.img} alt={props.alt}/>
+            <span>{props.message}</span>
             <p>like</p>
            </div>
 
